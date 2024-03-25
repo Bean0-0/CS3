de009404495ee896e45d79928e7e0d054bfeb190
Bean <bnasse2000@gmail.com>
2024-03-25 09:40:50 -0500

    Save

---
+++ BlackJack/src/BlackJack.java
@@ -0,0 +1,31 @@
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class BlackJack
+{
+	//add in Player instance variable
+	//add in Dealer instance variable
+
+	public BlackJack()
+	{
+
+
+	}
+
+	public void playGame()
+	{
+		Scanner keyboard = new Scanner(System.in);
+		char choice = 0;
+
+
+
+
+
+	}
+	
+	public static void main(String[] args)
+	{
+		BlackJack game = new BlackJack();
+		game.playGame();
+	}
+}
